9394d2749ae43d54965dc557ee885145516301f9
WataruSuzuki42 <w-suzuki@cresco.co.jp>
2020-06-10 16:05:08 +0900

    Minesweeper.javaを追加
    Signed-off-by: WataruSuzuki42 <w-suzuki@cresco.co.jp>

---
+++ 04_implementation/source/Minesweeper.java
@@ -0,0 +1,30 @@
+package マインスイーパー;
+
+public class Minesweeper {
+
+	private int mineTotal;
+
+	private static int cellsNum;
+
+	private Board board;
+
+	public Minesweeper(int mineTotal, int cellsNum) {
+		this.mineTotal = mineTotal;
+		this.cellsNum = cellsNum;
+	}
+
+	public void setAllMine(int x, int y) {
+		board.setMine(x, y, mineTotal);
+	}
+
+	public boolean removeCover(int x, int y) {
+		boolean hasMine = false;
+		
+		hasMine = board.removeCover(x, y);
+	}
+
+	public Cell[][] getCellList() {
+		return board.getCellList();
+	}
+
+}
