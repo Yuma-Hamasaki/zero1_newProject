00bd0631bc4fb22ef8cdfab4fd7e8ff1075f18fd
Yoshiki Imatake <y-imatake@cresco.co.jp>
2020-06-12 15:58:58 +0900

    startTime, getTimeの追加

--- 04_implementation/source/Minesweeper.java
+++ 04_implementation/source/Minesweeper.java
@@ -7,6 +7,7 @@ public class Minesweeper {
 	private int cellsNum;
 
 	private Board board;
+	private long time;
 
 	public Minesweeper(int mineTotal, int cellsNum) {
 		this.mineTotal = mineTotal;
@@ -28,6 +29,19 @@ public class Minesweeper {
 	
 	public int getMineTotal() {
 		return this.mineTotal;
+	}
+	
+	public void startTime() {
+		time = System.currentTimeMillis();
+	}
+	
+	public String getTime() {
+		Long currentTime = System.currentTimeMillis();
+		long diffMillis = currentTime - time;
+		int diff = (int)(diffMillis/1000);
+		int Min = diff/60;
+		int Sec = diff%60;
+		return Min + " : " + Sec;
 	}
 
 }
